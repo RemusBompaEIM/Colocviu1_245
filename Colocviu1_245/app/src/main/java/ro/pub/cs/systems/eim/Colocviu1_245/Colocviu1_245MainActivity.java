66ba51099d3f6e7c62812204f62a049e015f2db1
Remus Bompa <remus_stefan.bompa@stud.acs.pub.ro>
2020-04-09 14:05:26 +0300

    finished tasks A-C

--- Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -1,13 +1,84 @@
 package ro.pub.cs.systems.eim.Colocviu1_245;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.SyncStateContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Colocviu1_245MainActivity extends AppCompatActivity {
+
+    private static final int SECONDARY_ACTIVITY_REQUEST_CODE = 2;
+    private static final String SUM = "SUMA_SAVED";
+    private static final String LAST_TEXT = "TEXT SAVED";
+    private static String suma = "";
+    private static String last_text = "";
+    private static AppCompatActivity main = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_245_main);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(SUM))
+                suma = savedInstanceState.getString(SUM);
+            if (savedInstanceState.containsKey(LAST_TEXT))
+                last_text = savedInstanceState.getString(LAST_TEXT);
+
+        }
+
+        Button addB = (Button)findViewById(R.id.add);
+        addB.setOnClickListener(new AddButtonClickListener());
+        Button computeB = (Button)findViewById(R.id.compute);
+        computeB.setOnClickListener(new AddButtonClickListener());
+        main = this;
+    }
+
+     class AddButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+            switch(view.getId()) {
+                case R.id.add:
+                    String text = ((TextView)findViewById(R.id.all_terms)).getText().toString();
+                    String nr = ((EditText)findViewById(R.id.next_term)).getText().toString();
+                    if(!nr.isEmpty())
+                        ((TextView)findViewById(R.id.all_terms)).setText(text + "+" + nr);
+                    break;
+                case R.id.compute:
+                    String ints = ((TextView)findViewById(R.id.all_terms)).getText().toString();
+                    if(ints.equals(last_text))
+                        Toast.makeText(main, "The activity returned " + suma, Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), Colocviu1_245SecondaryActivity.class);
+                    intent.putExtra(Colocviu1_245SecondaryActivity.ALL_TERMS_KEY, ints);
+                    startActivityForResult(intent, SECONDARY_ACTIVITY_REQUEST_CODE);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == SECONDARY_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                suma = intent.getStringExtra(Colocviu1_245SecondaryActivity.RESULT_KEY);
+                Toast.makeText(this, "The activity returned " + suma, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(SUM, suma);
+        savedInstanceState.putString(LAST_TEXT, last_text);
     }
 }
--- Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -1,12 +1,30 @@
 package ro.pub.cs.systems.eim.Colocviu1_245;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
 
+    public static final String ALL_TERMS_KEY = "ALL_TERMS_KEY";
+    public static final String RESULT_KEY = "RESULT_KEY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String ints = intent.getStringExtra(ALL_TERMS_KEY);
+            String[] ls = ints.split("\\+");
+            int res = 0;
+            for(int i = 0; i<= ls.length; i++) {
+                res += Integer.parseInt(ls[i]);
+            }
+            Intent intentToParent = new Intent();
+            intent.putExtra(RESULT_KEY, res);
+            setResult(RESULT_OK, intentToParent);
+            finish();
+        }
     }
 }
