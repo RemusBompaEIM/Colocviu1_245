19fd3b805409eae816222498c282606517596db3
Remus Bompa <remus_stefan.bompa@stud.acs.pub.ro>
2020-04-09 14:04:04 +0300

    finished tasks A-C

---
+++ Colocviu1_245/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245SecondaryActivity.java
@@ -0,0 +1,12 @@
+package ro.pub.cs.systems.eim.Colocviu1_245;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Colocviu1_245SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
